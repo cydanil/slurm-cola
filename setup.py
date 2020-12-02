b1c8a4deff4417b6a610a11564d7405a3af906d9
Cyril Danilevski <cyril.danilevski@xfel.eu>
2020-12-02 21:25:08 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fin:
 
 setup(
      name='slurm-cola',
-     version='0.0.1',
+     version='0.0.2',
      author='Cyril Danilevski',
      author_email='cydanil@gmail.com',
      description='A tool to easily cancel slurm jobs belonging together',
